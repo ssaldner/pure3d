6c6868ec29bff33ef13f775fc5dd47212b24a58a
Dirk Roorda <dirk.roorda@di.huc.knaw.nl>
2022-08-10 09:31:14 +0200

    before pull

--- Website/app.py
+++ Website/app.py
@@ -1,49 +1,51 @@
-from flask import Flask, render_template,request,redirect,url_for #for flask
-import numpy as np
+from flask import Flask, render_template, request, url_for  # for flask
 import os
-import stat
 
-#create and configure app
+# create and configure app
 app = Flask(__name__)
-title ="PURE3D: An Infrastructure for Publication and Preservation of 3D Scholarship"
+title = "PURE3D: An Infrastructure for Publication and Preservation of 3D Scholarship"
 heading = "Pure 3D: Web Interface for Annotations"
 
+
 def redirect_url():
-    return request.args.get('next') or \
-           request.referrer or \
-           url_for('home') #redirect to index page
+    return (
+        request.args.get("next") or request.referrer or url_for("home")
+    )  # redirect to index page
+
 
 def select_edition():
-    data=[]
-    for root, dirs, files in os.walk(r".\editions"):
+    data = []
+    for root, dirs, files in os.walk("./editions"):
         for file in files:
-            if file == 'name.txt':
-                filename = os.path.join(root, file) 
-                #print(filename)
+            if file == "name.txt":
+                filename = os.path.join(root, file)
+                # print(filename)
 
                 with open(filename) as f:
                     data.append(f.readline())
-                    
-    data=np.array(data)
+
     return data
 
 
 @app.route("/")
 @app.route("/home")
-#Display home page
+# Display home page
 def home():
-    editions=select_edition()
-    return render_template('index.html',editions=editions)     
+    editions = select_edition()
+    return render_template("index.html", editions=editions)
+
 
 @app.route("/about")
-#Display the About page
+# Display the About page
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
+
 
 @app.route("/<n>")
 def edition_page(n):
-    n=select_edition()
-    return render_template('edition.html',n=n)
+    n = select_edition()
+    return render_template("edition.html", n=n)
+
 
 if __name__ == "__main__":
     app.run()
