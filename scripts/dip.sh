33b46d40d0a48a5c05534b412a3b8db4f4ada809
Dirk Roorda <dirk.roorda@di.huc.knaw.nl>
2022-08-30 16:52:16 +0200

    added flask voyager pilot: it works

--- scripts/dip.sh
+++ scripts/dip.sh
@@ -28,4 +28,10 @@ else
     export FLASK_DEBUG=0
 fi
 
-flask run
+flask run &
+pid=$!
+sleep 1
+python3 "$scriptdir/browser.py" http://127.0.0.1:5000
+trap "kill $pid" SIGINT
+echo "flask runs as process $pid"
+wait "$pid"
