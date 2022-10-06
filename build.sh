7fd83bb4a6de7028b4e18c8042ea4548d525f362
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-10-06 21:55:33 +0200

    build script will now push new image and pull on remote and restart stack remotely

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-git checkout containerize
-git pull
 source .env
 docker build --no-cache -t pure3dapp:${dockertag} \
   --build-arg gitlocation=${gitlocation} \
@@ -10,11 +8,10 @@ docker build --no-cache -t pure3dapp:${dockertag} \
   --build-arg DATA_DIR=${DATA_DIR} \
   --build-arg mysecret=${mysecret} \
   .
-
+docker images | grep pure3dapp
+sleep 5
 docker tag pure3dapp:${dockertag} registry.diginfra.net/vicd/pure3dapp:${dockertag}
+docker images | grep pure3dapp
 docker push registry.diginfra.net/vicd/pure3dapp:${dockertag}
 
-docker-compose --env-file /data/pure3dapp/.env -f /data/pure3dapp/docker-compose-acc.yml down
-docker pull registry.diginfra.net/vicd/pure3dapp:0.1
-docker-compose --env-file /data/pure3dapp/.env -f /data/pure3dapp/docker-compose-acc.yml up -d
-
+ssh pure3d.dev 'sudo /data/pure3dapp/restart.sh'
