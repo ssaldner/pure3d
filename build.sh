e12f7360f959fe547672be84125c75ad59be522a
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-10-06 18:52:47 +0200

    remote build

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+git pull
 source .env
-docker build --no-cache -t pure3dapp:${gittag} \
+docker build --no-cache -t pure3dapp:${dockertag} \
   --build-arg gitlocation=${gitlocation} \
   --build-arg gitbranch=${gitbranch} \
   --build-arg SECRET_FILE=${SECRET_FILE} \
   --build-arg DATA_DIR=${DATA_DIR} \
   --build-arg mysecret=${mysecret} \
   .
+
+docker tag pure3dapp:${dockertag} registry.diginfra.net/vicd/pure3dapp:${dockertag}
+docker push registry.diginfra.net/vicd/pure3dapp:${dockertag}
+
+docker-compose --env-file /data/pure3dapp/.env -f /data/pure3dapp/docker-compose-acc.yml down
+docker pull registry.diginfra.net/vicd/pure3dapp:0.1
+docker-compose --env-file /data/pure3dapp/.env -f /data/pure3dapp/docker-compose-acc.yml up -d
+
