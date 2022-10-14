4049f4c9ab09be7650ebf73b33b1ca0e7b98d4d2
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-10-14 14:50:42 +0200

    revise restart.sh

--- restart.sh
+++ restart.sh
@@ -2,6 +2,6 @@
 
 source .env
 docker pull registry.diginfra.net/vicd/pure3dapp:${dockertag}
-docker-compose --env-file /data/pure3dapp/.env -f /data/pure3dapp/docker-compose-acc.yml down
+docker-compose --env-file .env -f docker-compose-acc.yml down
 sleep 1
-docker-compose --env-file /data/pure3dapp/.env -f /data/pure3dapp/docker-compose-acc.yml up -d
+docker-compose --env-file .env -f docker-compose-acc.yml up -d
