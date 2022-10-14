5c54f536630c3fd2c225159f854dde0ee8af6976
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-10-14 09:39:44 +0200

    hard code git and branch

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-source .env
-ssh pure3d.dev 'sudo git clone -b ${gitbranch} ${gitlocation} /tmp/app && cd /tmp/app && sudo ./build.sh'
+ssh pure3d.dev 'sudo git clone -b https://github.com/CLARIAH/pure3d.git containerize /tmp/app && cd /tmp/app && sudo ./build.sh'
